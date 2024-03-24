493a96eadd1107b0e6d6de197abece15652a49e4
dgush <dgushch@gmail.com>
2024-03-24 13:05:16 +0400

    1.0.1

---
+++ handlers/scraping.py
@@ -0,0 +1,40 @@
+from pyrogram.types import Message
+from pyrogram.filters import chat
+
+from init import bot
+from data.chats import chats_flea_market_tbilisi, water_supply, gldanis_lair
+from utils.message_utils import remove_punctuation
+
+
+@bot.on_message(chat(list(chats_flea_market_tbilisi.values())))
+async def flea_market(client, msg: Message):
+    text = msg.text if msg.text else msg.caption
+    clear_text = remove_punctuation(text)
+
+    target = ['стол', 'стул', 'стулья', 'утюг']
+
+    for word in clear_text:
+        if word in target:
+            await bot.forward_messages(
+                chat_id=gldanis_lair,
+                from_chat_id=msg.chat.id,
+                message_ids=msg.id
+            )
+            break
+
+
+@bot.on_message(chat(water_supply))
+async def flea_market(client, msg: Message):
+    text = msg.text if msg.text else msg.caption
+    clear_text = remove_punctuation(text)
+
+    target = ['khizabavari',]
+
+    for word in clear_text:
+        if word in target:
+            await bot.forward_messages (
+                chat_id=gldanis_lair,
+                from_chat_id=msg.chat.id,
+                message_ids=msg.id
+            )
+            break
